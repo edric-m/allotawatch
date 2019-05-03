3821cfc94a25adee1f588359874e4276fcb662be
edric <edjomendoza@gmail.com>
2019-05-03 14:26:58 +1000

    update before drawer pages added

--- app/src/main/java/com/edsoft/allotawatch/MainActivity.java
+++ app/src/main/java/com/edsoft/allotawatch/MainActivity.java
@@ -192,17 +192,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_about) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_get_started) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_actions) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_usage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_contact) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_report_bugs) {
 
         }
 
